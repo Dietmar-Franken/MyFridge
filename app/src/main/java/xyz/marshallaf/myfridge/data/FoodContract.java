ded2105536bb55eb4a5f8b29f13f468a73b1bbcf
Andrew Marshall <marshall.a.f@gmail.com>
2017-01-23 22:58:12 -0600

    Set up initial database schema

---
+++ app/src/main/java/xyz/marshallaf/myfridge/data/FoodContract.java
@@ -0,0 +1,32 @@
+package xyz.marshallaf.myfridge.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Specifies the schema of the database.
+ *
+ * Created by Andrew Marshall on 1/23/2017.
+ */
+
+public final class FoodContract {
+    // non-instantiable
+    private FoodContract() {}
+
+    public static class FoodEntry implements BaseColumns {
+        public static final String TABLE_NAME = "food";
+
+        // column names
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_UNIT = "unit";
+        public static final String COLUMN_AMOUNT = "amount";
+        public static final String COLUMN_PRICE_PER = "priceper";
+        public static final String COLUMN_STORE = "store";
+        public static final String COLUMN_EXPIRATION = "expiration";
+        public static final String COLUMN_PHOTO = "photo";
+
+        // constants for unit
+        public static final int UNIT_ML = 0;
+        public static final int UNIT_MG = 1;
+        public static final int UNIT_ITEM = 2;
+    }
+}
