a3414c21a865a31a3e49ea4dbe351e5aab7bb962
Andrew Marshall <marshall.a.f@gmail.com>
2017-01-24 20:02:57 -0600

    Add unit preference in addition to unit type

--- app/src/main/java/xyz/marshallaf/myfridge/data/FoodContract.java
+++ app/src/main/java/xyz/marshallaf/myfridge/data/FoodContract.java
@@ -18,7 +18,8 @@ public final class FoodContract {
         // column names
         public static final String _ID = BaseColumns._ID;
         public static final String COLUMN_NAME = "name";
-        public static final String COLUMN_UNIT = "unit";
+        public static final String COLUMN_UNIT_TYPE = "unit_type";
+        public static final String COLUMN_UNIT_PREF = "unit_pref";
         public static final String COLUMN_AMOUNT = "amount";
         public static final String COLUMN_PRICE_PER = "priceper";
         public static final String COLUMN_STORE = "store";
@@ -27,7 +28,7 @@ public final class FoodContract {
 
         // constants for unit
         public static final int UNIT_ITEM = 0;
-        public static final int UNIT_ML = 1;
-        public static final int UNIT_MG = 2;
+        public static final int UNIT_VOL = 1;
+        public static final int UNIT_MASS = 2;
     }
 }
