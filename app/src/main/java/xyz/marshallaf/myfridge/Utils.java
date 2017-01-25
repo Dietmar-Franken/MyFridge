706b4acc27ae22c6739b3b3e70b8d8ba6e43969f
Andrew Marshall <marshall.a.f@gmail.com>
2017-01-24 20:25:59 -0600

    Stub for convert method

---
+++ app/src/main/java/xyz/marshallaf/myfridge/Utils.java
@@ -0,0 +1,16 @@
+package xyz.marshallaf.myfridge;
+
+/**
+ * Created by Andrew Marshall on 1/24/2017.
+ */
+
+public final class Utils {
+    // not instantiable
+    private Utils() {}
+
+    // convert units from stored to preference
+    public static float convert(float amount, int units) {
+        // TODO: implement this
+        return amount;
+    }
+}
