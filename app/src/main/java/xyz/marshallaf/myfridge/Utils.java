cb3252d0a7a9cef4e520263ad6927a440a7cbe0f
Andrew Marshall <marshall.a.f@gmail.com>
2017-01-28 21:44:51 -0600

    Finish convert method

--- app/src/main/java/xyz/marshallaf/myfridge/Utils.java
+++ app/src/main/java/xyz/marshallaf/myfridge/Utils.java
@@ -1,5 +1,7 @@
 package xyz.marshallaf.myfridge;
 
+import xyz.marshallaf.myfridge.data.FoodContract;
+
 /**
  * Created by Andrew Marshall on 1/24/2017.
  */
@@ -8,9 +10,82 @@ public final class Utils {
     // not instantiable
     private Utils() {}
 
-    // convert units from stored to preference
-    public static float convert(float amount, int units) {
-        // TODO: implement this
-        return amount;
+    // volume conversions
+    private static double ML_IN_L = 1000.0;
+    private static double ML_IN_FLOZ = 29.5735295625;
+    private static double ML_IN_PINT = 473.176473;
+    private static double ML_IN_QUART = 946.352946;
+    private static double ML_IN_GALLON = 3785.411784;
+    private static double ML_IN_TSP = 4.92892159375;
+    private static double ML_IN_TBSP = 14.78676478125;
+    private static double ML_IN_CUP = 236.5882365;
+
+    // mass conversions
+    private static double G_IN_MG = 0.001;
+    private static double G_IN_KG = 1000.0;
+    private static double G_IN_OZ = 28.349523125;
+    private static double G_IN_POUND = 453.59237;
+
+    // convert units
+    public static double convert(double amount, int units, boolean forStorage) {
+        double conversionFactor = 1;
+        if (units == 0) {
+            // items
+            return amount;
+        }
+        else if (units >= 200 && units < 300) {
+            // volume, starting with ml
+            switch (units) {
+                case FoodContract.FoodEntry.UNIT_ML:
+                    break;
+                case FoodContract.FoodEntry.UNIT_L:
+                    conversionFactor = ML_IN_L;
+                    break;
+                case FoodContract.FoodEntry.UNIT_FLOZ:
+                    conversionFactor = ML_IN_FLOZ;
+                    break;
+                case FoodContract.FoodEntry.UNIT_PINT:
+                    conversionFactor = ML_IN_PINT;
+                    break;
+                case FoodContract.FoodEntry.UNIT_QUART:
+                    conversionFactor = ML_IN_QUART;
+                    break;
+                case FoodContract.FoodEntry.UNIT_GALLON:
+                    conversionFactor = ML_IN_GALLON;
+                    break;
+                case FoodContract.FoodEntry.UNIT_TSP:
+                    conversionFactor = ML_IN_TSP;
+                    break;
+                case FoodContract.FoodEntry.UNIT_TBSP:
+                    conversionFactor = ML_IN_TBSP;
+                    break;
+                case FoodContract.FoodEntry.UNIT_CUP:
+                    conversionFactor = ML_IN_CUP;
+                    break;
+            }
+        } else if (units >= 300 && units < 400) {
+            // mass, starting with g
+            switch (units) {
+                case FoodContract.FoodEntry.UNIT_GRAM:
+                    return amount;
+                case FoodContract.FoodEntry.UNIT_KG:
+                    conversionFactor = G_IN_KG;
+                    break;
+                case FoodContract.FoodEntry.UNIT_MG:
+                    conversionFactor = G_IN_MG;
+                    break;
+                case FoodContract.FoodEntry.UNIT_OZ:
+                    conversionFactor = G_IN_OZ;
+                    break;
+                case FoodContract.FoodEntry.UNIT_POUND:
+                    conversionFactor = G_IN_POUND;
+                    break;
+            }
+        }
+        if (forStorage) {
+            return amount * conversionFactor;
+        } else {
+            return amount / conversionFactor;
+        }
     }
 }
