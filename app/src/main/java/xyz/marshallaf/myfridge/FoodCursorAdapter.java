e3ab7b4943d6ef5f8127019126590decfa18fc1f
Andrew Marshall <marshall.a.f@gmail.com>
2017-02-11 18:04:37 -0600

    Restrict to package-private

--- app/src/main/java/xyz/marshallaf/myfridge/FoodCursorAdapter.java
+++ app/src/main/java/xyz/marshallaf/myfridge/FoodCursorAdapter.java
@@ -2,12 +2,13 @@ package xyz.marshallaf.myfridge;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.icu.math.BigDecimal;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 import xyz.marshallaf.myfridge.data.FoodContract.FoodEntry;
 
@@ -17,11 +18,11 @@ import xyz.marshallaf.myfridge.data.FoodContract.FoodEntry;
  * Created by Andrew Marshall on 1/24/2017.
  */
 
-public class FoodCursorAdapter extends CursorAdapter {
+class FoodCursorAdapter extends CursorAdapter {
 
-    Context mContext;
+    private Context mContext;
 
-    public FoodCursorAdapter(Context context, Cursor c) {
+    FoodCursorAdapter(Context context, Cursor c) {
         super(context, c, 0);
         mContext = context;
     }
@@ -39,7 +40,7 @@ public class FoodCursorAdapter extends CursorAdapter {
 
         String unitString = Utils.getUnitString(units, mContext);
         double amountConverted = Utils.convert(amount, units, false, mContext);
-        java.math.BigDecimal amountRounded = new java.math.BigDecimal(amountConverted).setScale(2, BigDecimal.ROUND_HALF_UP);
+        BigDecimal amountRounded = new BigDecimal(amountConverted).setScale(2, BigDecimal.ROUND_HALF_UP);
 
         ((TextView) view.findViewById(R.id.food_item_name)).setText(foodName);
         ((TextView) view.findViewById(R.id.food_item_amount)).setText(amountRounded.toString() + " " + unitString);
